92e8d7b6d508b5627c8c1fcefd7bd218f99eb687
Artur Oliveira Silva Alves <arturoliveira88@outlook.com>
2022-07-10 11:51:30 -0300

    testando 4

--- velha.py
+++ velha.py
@@ -13,13 +13,24 @@ def jogo(matriz):
                 cont_O += 1
     
     
-    # CRITERIOS PARA ESTAR INDEFINIDO -----------------------
+    # CRITERIOS PARA ESTAR INDEFINIDO ------------------------------
     
     if cont_X + cont_O < 5:
         return -1
     
+    # CRITERIO PARA X VENCER ---------------------------------------------
+    
     if matriz[0][0] == 2 and matriz[0][1] == 2 and matriz[0][2] == 2:
         return 1
     
-    #if matriz
+    elif matriz[1][0] == 2 and matriz[1][1] == 2 and matriz[1][2] == 2:
+        return 1
     
+    elif matriz[2][0] == 2 and matriz[2][1] == 2 and matriz[2][2] == 2:
+        return 1
+    
+    elif matriz[0][0] == 2 and matriz[1][1] == 2 and matriz[2][2] == 2:
+        return 1
+    
+    elif matriz[0][2] == 2 and matriz[1][1] == 2 and matriz[2][0] == 2:
+        return 1
--- velha_test.py
+++ velha_test.py
@@ -1,17 +1,21 @@
 from velha import jogo
 
+
 def test_1():
-    assert jogo([[2,2,2],[2,1,1],[0,1,1]]) == 1
-    
+    assert jogo([[2,2,2],[2,1,1],[0,1,1]]) == 1 # HORIZONTAL
+
 def test_2():
-    assert jogo([[1,2,2],[0,1,2],[0,0,2]]) == 1
+    assert jogo([[2,1,1],[0,2,1],[1,0,2]]) == 1 # DIAGONAL
     
+def test_3():
+    assert jogo([[1,2,2],[0,1,2],[0,0,2]]) == 1 # VERTICAL
+'''    
 def test_3():
     assert jogo([[2,1,2],[2,1,2],[1,2,1]]) == 0
     
 def test_4():
     assert jogo([[2,0,0],[0,0,0],[0,0,0]]) == -1
-'''    
+    
 def test_5():
     assert jogo([[],[],[]])
 '''
