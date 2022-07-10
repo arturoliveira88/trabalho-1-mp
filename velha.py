fd57a6aa2a61d33132950e585a1850aa947d9b78
Artur Oliveira Silva Alves <arturoliveira88@outlook.com>
2022-07-10 02:57:41 -0300

    testando 3

--- velha.py
+++ velha.py
@@ -1,5 +1,25 @@
 def jogo(matriz):
     
+    # CONTAGEM --------------------------------------------------
+    
+    cont_X = 0
+    cont_O = 0
+    
+    for i in matriz: # CONTA OS SIMBOLOS
+        for j in i:
+            if j == 2:
+                cont_X += 1
+            if j == 1:
+                cont_O += 1
+    
+    
+    # CRITERIOS PARA ESTAR INDEFINIDO -----------------------
+    
+    if cont_X + cont_O < 5:
+        return -1
+    
     if matriz[0][0] == 2 and matriz[0][1] == 2 and matriz[0][2] == 2:
         return 1
+    
+    #if matriz
     
--- velha_test.py
+++ velha_test.py
@@ -4,13 +4,14 @@ def test_1():
     assert jogo([[2,2,2],[2,1,1],[0,1,1]]) == 1
     
 def test_2():
-    assert jogo([[1,2,2],[0,1,2],[0,0,1]]) == 2
+    assert jogo([[1,2,2],[0,1,2],[0,0,2]]) == 1
     
 def test_3():
     assert jogo([[2,1,2],[2,1,2],[1,2,1]]) == 0
-
-def test_4():
-    assert jogo([[],[],[]])
     
+def test_4():
+    assert jogo([[2,0,0],[0,0,0],[0,0,0]]) == -1
+'''    
 def test_5():
     assert jogo([[],[],[]])
+'''
