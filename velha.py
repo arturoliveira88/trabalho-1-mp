8df8c886a04c9ca78fba6fbe72b6f4d5b9bb2fc6
Artur Oliveira Silva Alves <arturoliveira88@outlook.com>
2022-07-10 11:58:57 -0300

    criterios para x vencer

--- velha.py
+++ velha.py
@@ -18,19 +18,28 @@ def jogo(matriz):
     if cont_X + cont_O < 5:
         return -1
     
-    # CRITERIO PARA X VENCER ---------------------------------------------
+    # CRITERIOS PARA X VENCER ---------------------------------------------
     
-    if matriz[0][0] == 2 and matriz[0][1] == 2 and matriz[0][2] == 2:
+    if matriz[0][0] == 2 and matriz[0][1] == 2 and matriz[0][2] == 2: # HORIZONTAL
         return 1
     
-    elif matriz[1][0] == 2 and matriz[1][1] == 2 and matriz[1][2] == 2:
+    elif matriz[1][0] == 2 and matriz[1][1] == 2 and matriz[1][2] == 2: # HORIZONTAL
         return 1
     
-    elif matriz[2][0] == 2 and matriz[2][1] == 2 and matriz[2][2] == 2:
+    elif matriz[2][0] == 2 and matriz[2][1] == 2 and matriz[2][2] == 2: # HORIZONTAL
         return 1
     
-    elif matriz[0][0] == 2 and matriz[1][1] == 2 and matriz[2][2] == 2:
+    elif matriz[0][0] == 2 and matriz[1][0] == 2 and matriz[2][0] == 2: # VERTICAL
         return 1
     
-    elif matriz[0][2] == 2 and matriz[1][1] == 2 and matriz[2][0] == 2:
+    elif matriz[0][1] == 2 and matriz[1][1] == 2 and matriz[2][1] == 2: # VERTICAL
+        return 1
+    
+    elif matriz[0][2] == 2 and matriz[1][2] == 2 and matriz[2][2] == 2: # VERTICAL
+        return 1
+    
+    elif matriz[0][0] == 2 and matriz[1][1] == 2 and matriz[2][2] == 2: # DIAGONAL
+        return 1
+    
+    elif matriz[0][2] == 2 and matriz[1][1] == 2 and matriz[2][0] == 2: # DIAGONAL
         return 1
